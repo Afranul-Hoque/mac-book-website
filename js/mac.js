d0b08eeb56b8b9d69ac83ed343c9fb46563c7354
Afranul Hoque <hoqueafranul2021@gmail.com>
2021-08-21 19:18:07 +0600

    final touch

--- js/mac.js
+++ js/mac.js
@@ -1,29 +1,131 @@
 // choose memory and added cost
 
-document.getElementById('memory1-button').addEventListener('click', function () {
-    const memoryPrice = document.getElementById('memory-price');
-    const memoryNewPrice = 0;
-    memoryPrice.innerText = memoryNewPrice;
+function getmemoryPrice(product, price) {
+    const memoryPrice = document.getElementById(product + '-price');
+    if (price == 0) {
+        memoryPrice.innerText = price;
+    }
+    else if (price == 180) {
+        memoryPrice.innerText = price;
 
+    }
     const totalPriceIncrease = document.getElementById('total-price');
-    const newTotalPrice = 1299 + parseInt(memoryNewPrice);
+    const newTotalPrice = 1299 + parseInt(memoryPrice.innerText);
     totalPriceIncrease.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
+    TotalPrice()
+
+};
+
+
+// function for storage choose and cost
+
+function getStoragePrice(product, price) {
+    const storagePrice = document.getElementById(product + '-price');
+    if (price == 0) {
+        storagePrice.innerText = price;
+    }
+    else if (price == 100) {
+        storagePrice.innerText = price;
+    }
+    else if (price == 180) {
+        storagePrice.innerText = price;
+    }
+    const storagePriceIncrease = document.getElementById('total-price');
+    const newTotalPrice = 1299 + parseInt(storagePrice.innerText);
+    storagePriceIncrease.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
+    TotalPrice()
+};
+
+// function choose  delivery option and added cost
+
+
+function getDeliveryPrice(product, price) {
+    const deliveryPrice = document.getElementById(product + '-price');
+
+    if (price == 0) {
+        deliveryPrice.innerText = price;
+    }
+    else if (price == 20) {
+        deliveryPrice.innerText = price;
+    }
+
+    const deliveryPriceIncrease = document.getElementById('total-price');
+    const newTotalPrice = 1299 + parseInt(deliveryPrice.innerText);
+    deliveryPriceIncrease.innerText = newTotalPrice;
+
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
+    TotalPrice()
+};
+
+// function getValue(){
+//     const totalMemory = document.getElementById('memory-price');
+//     const totalMemoryPrice = parseInt(totalMemory.value);
+
+// }
+
+function TotalPrice() {
+    const totalMemory = document.getElementById('memory-price');
+    const totalMemoryPrice = parseInt(totalMemory.innerText);
+
+    const totalStorage = document.getElementById('storage-price');
+    const totalStoragePrice = parseInt(totalStorage.innerText);
+
+    const totaldelivery = document.getElementById('delivery-price');
+    const totaldeliveryPrice = parseInt(totaldelivery.innerText);
+
+
+    const lastMacprice = document.getElementById('fixed-price');
+
+    Macprice = 1299 + totalMemoryPrice + totalStoragePrice + totaldeliveryPrice;
+
+    const MacTotal = document.getElementById('total-price');
+    MacTotal.innerText = Macprice;
+
     const pomoTotal = document.getElementById('pomo-Total');
-    pomoTotal.innerText = newTotalPrice;
+    pomoTotal.innerText = Macprice;
+
+
+
+
+}
+
+
+
+
+
+document.getElementById('memory1-button').addEventListener('click', function () {
+    // const memoryPrice = document.getElementById('memory-price');
+    // const memoryNewPrice = 0;
+    // memoryPrice.innerText = memoryNewPrice;
+    getmemoryPrice('memory', 0);
+
+    // const totalPriceIncrease = document.getElementById('total-price');
+    // const newTotalPrice = 1299 + parseInt(memoryNewPrice);
+    // totalPriceIncrease.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
+
+
 
 });
 
 document.getElementById('memory2-button').addEventListener('click', function () {
-    const memoryPrice = document.getElementById('memory-price');
-    const memoryNewPrice = 180;
-    memoryPrice.innerText = memoryNewPrice;
+    // const memoryPrice = document.getElementById('memory-price');
+    // const memoryNewPrice = 180;
+    // memoryPrice.innerText = memoryNewPrice;
+    getmemoryPrice('memory', 180);
 
-    const totalPriceIncrease = document.getElementById('total-price');
-    const newTotalPrice = 1299 + parseInt(memoryNewPrice);
-    totalPriceIncrease.innerText = newTotalPrice;
+    // const totalPriceIncrease = document.getElementById('total-price');
+    // const newTotalPrice = 1299 + parseInt(memoryNewPrice);
+    // totalPriceIncrease.innerText = newTotalPrice;
 
-    const pomoTotal = document.getElementById('pomo-Total');
-    pomoTotal.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
 
 });
 
@@ -31,42 +133,42 @@ document.getElementById('memory2-button').addEventListener('click', function () 
 // choose Storage and added cost
 
 document.getElementById('storage1-button').addEventListener('click', function () {
-    const storagePrice = document.getElementById('storage-price');
-    const storageNewPrice = 0;
-    storagePrice.innerText = storageNewPrice;
+    // const storagePrice = document.getElementById('storage-price');
+    // const storageNewPrice = 0;
+    // storagePrice.innerText = storageNewPrice;
+    getStoragePrice('storage', 0);
 
-    const storagePriceIncrease = document.getElementById('total-price');
-    const newTotalPrice = 1299 + parseInt(storageNewPrice);
-    storagePriceIncrease.innerText = newTotalPrice;
-    const pomoTotal = document.getElementById('pomo-Total');
-    pomoTotal.innerText = newTotalPrice;
+
+    // const storagePriceIncrease = document.getElementById('total-price');
+    // const newTotalPrice = 1299 + parseInt(storageNewPrice);
+    // storagePriceIncrease.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
 
 });
 
 
 document.getElementById('storage2-button').addEventListener('click', function () {
-    const storagePrice = document.getElementById('storage-price');
-    const storageNewPrice = 100;
-    storagePrice.innerText = storageNewPrice;
+    getStoragePrice('storage', 100);
 
-    const storagePriceIncrease = document.getElementById('total-price');
-    const newTotalPrice = 1299 + parseInt(storageNewPrice);
-    storagePriceIncrease.innerText = newTotalPrice;
-    const pomoTotal = document.getElementById('pomo-Total');
-    pomoTotal.innerText = newTotalPrice;
+
+    // const storagePriceIncrease = document.getElementById('total-price');
+    // const newTotalPrice = 1299 + parseInt(storageNewPrice);
+    // storagePriceIncrease.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
 
 });
 
 document.getElementById('storage3-button').addEventListener('click', function () {
-    const storagePrice = document.getElementById('storage-price');
-    const storageNewPrice = 180;
-    storagePrice.innerText = storageNewPrice;
+    getStoragePrice('storage', 180);
 
-    const storagePriceIncrease = document.getElementById('total-price');
-    const newTotalPrice = 1299 + parseInt(storageNewPrice);
-    storagePriceIncrease.innerText = newTotalPrice;
-    const pomoTotal = document.getElementById('pomo-Total');
-    pomoTotal.innerText = newTotalPrice;
+
+    // const storagePriceIncrease = document.getElementById('total-price');
+    // const newTotalPrice = 1299 + parseInt(storageNewPrice);
+    // storagePriceIncrease.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
 
 });
 
@@ -74,30 +176,32 @@ document.getElementById('storage3-button').addEventListener('click', function ()
 // choose  delivery option and added cost
 
 document.getElementById('delivery1-button').addEventListener('click', function () {
-    const deliveryPrice = document.getElementById('delivery-price');
-    const newDeliveryPrice = 0;
-    deliveryPrice.innerText = newDeliveryPrice;
+    // const deliveryPrice = document.getElementById('delivery-price');
+    // const newDeliveryPrice = 0;
+    // deliveryPrice.innerText = newDeliveryPrice;
+    getDeliveryPrice('delivery', 0);
 
-    const deliveryPriceIncrease = document.getElementById('total-price');
-    const newTotalPrice = 1299 + parseInt(newDeliveryPrice);
-    deliveryPriceIncrease.innerText = newTotalPrice;
+    // const deliveryPriceIncrease = document.getElementById('total-price');
+    // const newTotalPrice = 1299 + parseInt(newDeliveryPrice);
+    // deliveryPriceIncrease.innerText = newTotalPrice;
 
-    const pomoTotal = document.getElementById('pomo-Total');
-    pomoTotal.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
 });
 
 
 document.getElementById('delivery2-button').addEventListener('click', function () {
-    const deliveryPrice = document.getElementById('delivery-price');
-    const newDeliveryPrice = 20;
-    deliveryPrice.innerText = newDeliveryPrice;
+    // const deliveryPrice = document.getElementById('delivery-price');
+    // const newDeliveryPrice = 20;
+    // deliveryPrice.innerText = newDeliveryPrice;
+    getDeliveryPrice('delivery', 20);
 
-    const deliveryPriceIncrease = document.getElementById('total-price');
-    const newTotalPrice = 1299 + parseInt(newDeliveryPrice);
-    deliveryPriceIncrease.innerText = newTotalPrice;
+    // const deliveryPriceIncrease = document.getElementById('total-price');
+    // const newTotalPrice = 1299 + parseInt(newDeliveryPrice);
+    // deliveryPriceIncrease.innerText = newTotalPrice;
 
-    const pomoTotal = document.getElementById('pomo-Total');
-    pomoTotal.innerText = newTotalPrice;
+    // const pomoTotal = document.getElementById('pomo-Total');
+    // pomoTotal.innerText = newTotalPrice;
 
 });
 
