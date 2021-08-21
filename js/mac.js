2035851dc3946363fd598addcf916dc957c9f682
Afranul Hoque <hoqueafranul2021@gmail.com>
2021-08-21 10:54:47 +0600

    storage added

--- js/mac.js
+++ js/mac.js
@@ -2,27 +2,61 @@
 
 document.getElementById('memory1-button').addEventListener('click', function () {
     const memoryPrice = document.getElementById('memory-price');
-    const memoryNewPrice = parseInt(memoryPrice.innerText) + 0;
+    const memoryNewPrice = 0;
     memoryPrice.innerText = memoryNewPrice;
 
     const totalPriceIncrease = document.getElementById('total-price');
-    const newTotalPrice = parseInt(totalPriceIncrease.innerText) + parseInt(memoryNewPrice);
+    const newTotalPrice = 1299 + parseInt(memoryNewPrice);
     totalPriceIncrease.innerText = newTotalPrice;
 
 });
 
 document.getElementById('memory2-button').addEventListener('click', function () {
     const memoryPrice = document.getElementById('memory-price');
-    const memoryNewPrice = parseInt(memoryPrice.innerText) + 180;
-    if (memoryNewPrice <= 180) {
-        memoryPrice.innerText = memoryNewPrice;
+    const memoryNewPrice = 180;
+    memoryPrice.innerText = memoryNewPrice;
 
-        const totalPriceIncrease = document.getElementById('total-price');
-        const newTotalPrice = parseInt(totalPriceIncrease.innerText) + parseInt(memoryNewPrice);
-        totalPriceIncrease.innerText = newTotalPrice;
-    }
+    const totalPriceIncrease = document.getElementById('total-price');
+    const newTotalPrice = 1299 + parseInt(memoryNewPrice);
+    totalPriceIncrease.innerText = newTotalPrice;
+
 
 });
 
 
+// choose Storage and added cost
+
+document.getElementById('storage1-button').addEventListener('click', function () {
+    const storagePrice = document.getElementById('storage-price');
+    const storageNewPrice = 0;
+    storagePrice.innerText = storageNewPrice;
+
+    const storagePriceIncrease = document.getElementById('total-price');
+    const newTotalPrice = 1299 + parseInt(storageNewPrice);
+    storagePriceIncrease.innerText = newTotalPrice;
+
+});
+
+
+document.getElementById('storage2-button').addEventListener('click', function () {
+    const storagePrice = document.getElementById('storage-price');
+    const storageNewPrice = 100;
+    storagePrice.innerText = storageNewPrice;
+
+    const storagePriceIncrease = document.getElementById('total-price');
+    const newTotalPrice = 1299 + parseInt(storageNewPrice);
+    storagePriceIncrease.innerText = newTotalPrice;
+
+});
+
+document.getElementById('storage3-button').addEventListener('click', function () {
+    const storagePrice = document.getElementById('storage-price');
+    const storageNewPrice = 180;
+    storagePrice.innerText = storageNewPrice;
+
+    const storagePriceIncrease = document.getElementById('total-price');
+    const newTotalPrice = 1299 + parseInt(storageNewPrice);
+    storagePriceIncrease.innerText = newTotalPrice;
+
+});
 
